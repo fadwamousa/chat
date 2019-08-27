6001133334df6509d06c9f4b3c688bd7e12f1263
fadwa <fadwaana99@gmail.com>
2019-08-27 09:00:47 +0200

    chat application

---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="list-group-item"><slot></slot></li>
+</template>
+
+<script>
+    export default {
+        name: "MessageComponent.vue"
+    }
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title>Chat Application</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+
+<br> <br> <br> <br>
+
+<div class="container">
+    <div class="row" id="app">
+        <div class="offset-4 col-4 offset-sm-1 col-sm-10">
+            <li class="list-group-item active">Chat Room</li>
+            <ul class="list-group">
+
+                <message v-for="value in chat.message">
+                    @{{ value }}
+                </message>
+
+            </ul>
+
+
+
+            <input type="text"
+                   class="form-control"
+                   placeholder="Type your message here..."
+                   v-model="message" @keyup.enter="send()">
+            <br>
+        </div>
+    </div>
+</div>
+
+
+
+<script src="{{asset('js/app.js')}}"></script>
+
+</body>
+</html>
